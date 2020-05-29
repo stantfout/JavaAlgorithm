f0564cea3d6bf079bd8b314a5235a69630ccd60a
wang <986093257@qq.com>
2020-05-29 22:05:52 +0800

    125,152,680

---
+++ src/com/usth/leetcode/leetcode101_200/LeetCode_125.java
@@ -0,0 +1,24 @@
+package com.usth.leetcode.leetcode101_200;
+
+public class LeetCode_125 {
+    public boolean isPalindrome(String s) {
+        int n = s.length();
+        int i = 0;
+        int j = n - 1;
+        while (i < j) {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+            }
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            }
+            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+                return false;
+            }
+            i++;
+            j--;
+
+        }
+        return true;
+    }
+}
