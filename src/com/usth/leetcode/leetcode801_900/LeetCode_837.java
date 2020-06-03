2adc68be5a1b33c7075e587bf19fbd46a187fbb5
wang <986093257@qq.com>
2020-06-03 12:28:51 +0800

    837

---
+++ src/com/usth/leetcode/leetcode801_900/LeetCode_837.java
@@ -0,0 +1,27 @@
+package com.usth.leetcode.leetcode801_900;
+
+public class LeetCode_837 {
+    public double new21Game(int N, int K, int W) {
+        //特判K=0时结果为1
+        if (K == 0) {
+            return 1;
+        }
+        double[] dp = new double[N + 1];
+        double[] sum = new double[N + 1];
+        dp[0] = 1;
+        sum[0] = 1;
+        double tmp = (1.0 / W);
+        for (int i = 1; i <= N; i++) {
+            //这里通过前缀和求解时要注意边界问题
+            double sum1 = Math.max(0, i - W) - 1 >= 0 ? sum[Math.max(0, i - W) - 1] : 0;
+            double sum2 = sum[Math.min(i - 1, K - 1)];
+            dp[i] = (sum2 - sum1) * tmp;
+            sum[i] = sum[i - 1] + dp[i];
+        }
+        double res = 0;
+        for (int i = K; i <= N; ++i) {
+            res += dp[i];
+        }
+        return res;
+    }
+}
